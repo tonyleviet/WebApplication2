c045944c4a7cd3377b60eef35be389790cc104dc
Viet Le <tony.leviet@gmail.com>
2017-07-05 10:13:45 +0700

    test 2

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsFalse(false);
+            Assert.IsFalse(false); 
         }
 
         [TestMethod]
