6186661d02c5035e016b53eabcbff2026fcf634a
Viet Le <tony.leviet@gmail.com>
2017-07-05 10:04:48 +0700

    Add project files.

---
+++ WebApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication2
+{
+    public interface IProgram
+    {
+        void PrintString(string v);
+
+    }
+    public class Program : IProgram
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+            host.Run();
+        }
+
+        public void PrintString(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
